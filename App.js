f601b666520e78db78fa83ff23d82d52375cf237
Talha Akhter <talhaakhter01@gmail.com>
2020-12-11 05:16:43 -0600

    second commit

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('hello this is awesome');
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
     <View style={styles.container}>
       <Text>{ outputText }</Text>
-      <Button title='Change Text' onPress={ () => setOutputText('The text changed')} />
-      <StatusBar style="auto"s />
+      <Button title='Change Text' onPress={ () => setOutputText('The text changed!')} />
+      <StatusBar style="auto"/>
     </View>
   );
 }
